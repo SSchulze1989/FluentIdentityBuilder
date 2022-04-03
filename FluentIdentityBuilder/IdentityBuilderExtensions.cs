6c0ca2cc6c8419b7cc6f96f167daa5c2401e2b45
Simon Schulze <simonschulze2589@hotmail.de>
2022-04-03 12:35:52 +0200

    Add extension methods

---
+++ FluentIdentityBuilder/IdentityBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentIdentityBuilder
+{
+    public static class IdentityBuilderExtensions
+    {
+        public static IIdentityBuilder<T> WithClaims<T>(this IIdentityBuilder<T> builder, IEnumerable<Claim> claims)
+        {
+            foreach(var claim in claims)
+            {
+                builder.WithClaim(claim.Type, claim.Value);
+            }
+            return builder;
+        }
+
+        public static IIdentityBuilder<T> WithRoles<T>(this IIdentityBuilder<T> builder, IEnumerable<string> roles)
+        {
+            foreach(var role in roles)
+            {
+                builder.WithRole(role);
+            }
+            return builder;
+        }
+    }
+}
---
+++ UnitTests/TestExtensions.cs
@@ -0,0 +1,39 @@
+﻿using FluentIdentityBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TestExtensions
+    {
+        private Claim claim1 = new Claim("Type1", "Value1");
+        private Claim claim2 = new Claim("Type2", "Value2");
+        private const string role1 = "Role1";
+        private const string role2 = "Role2";
+
+        [Fact]
+        public void TestWithClaims()
+        {
+            var identity = IdentityBuilders.BuildIdentity()
+                .WithClaims(new Claim[] { claim1, claim2 })
+                .Create();
+            Assert.True(identity.HasClaim(claim1.Type, claim1.Value));
+            Assert.True(identity.HasClaim(claim2.Type, claim2.Value));
+        }
+
+        [Fact]
+        public void TestWithRoles()
+        {
+            var principal = IdentityBuilders.BuildPrincipal()
+                .WithRoles(new string[] { role1, role2 })
+                .Create();
+            Assert.True(principal.IsInRole(role1));
+            Assert.True(principal.IsInRole(role2));
+        }
+    }
+}
